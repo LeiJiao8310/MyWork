cc433eae1d1f13be86238a8f635a823973458dbe
LeiJiao <jiaorichard30@gmail.com>
2018-09-01 06:14:22 -0700

    pa2 commit

---
+++ cmps12a/pa2/GCD.java
@@ -0,0 +1,43 @@
+/**LEI JIAO
+ * Program3
+ * ljiao
+ * This program calculates the greatest common divisor.
+ */
+
+
+import java.util.Scanner;
+class GCD{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        System.out.print("Enter a positive integer: ");
+        while(true){
+            if(sc.hasNextInt()){
+                x = sc.nextInt();
+                if( x>0 ) break;
+            }else{
+                sc.next();
+            }
+            System.out.print("Please enter a positive integer: ");
+        }
+        System.out.print("Enter another positive integer:");
+
+        while(true){
+            if(sc.hasNextInt()){
+                y = sc.nextInt();
+                if( y>0 ) break;
+            }else{
+                sc.next();
+            }
+            System.out.print("Please enter a positive integer: ");
+        }
+
+
+        System.out.println("The GCD of "+x+" and "+y+" is " + gcd(x,y)+" .");
+
+    }
+    private static int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+
+}
